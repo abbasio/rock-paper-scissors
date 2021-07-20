71c58291fcd97caf01cb7330dc1a949148f063fc
abbasio <omarabbasi16@gmail.com>
2021-07-20 15:41:57 -0400

    Added playRound function

--- RPS.js
+++ RPS.js
@@ -6,4 +6,39 @@ function computerPlay(arr) {
     console.log(arr[Math.floor(Math.random() * arr.length)]);
 }
 
-computerPlay(rps);
+const playerSelection = "rock";
+const computerSelection = (computerPlay(rps));
+
+function playRound(playerSelection, computerSelection) {
+    if (computerSelection === "rock") {
+        if (playerSelection.toLowerCase() === "paper") {
+            console.log("You win!");
+        }
+        else if (playerSelection.toLowerCase() === "scissors") {
+            console.log("You lose.");
+        }
+        else console.log("Invalid input");
+    }
+    if (computerSelection === "paper") {
+        if (playerSelection.toLowerCase() === "scissors") {
+            console.log("You win!");
+        }
+        else if (playerSelection.toLowerCase() === "rock") {
+            console.log("You lose.");
+        }
+        else console.log("Invalid input");
+    }
+    if (computerSelection === "scissors") {
+        if (playerSelection.toLowerCase() === "rock") {
+            console.log("You win!");
+        }
+        else if (playerSelection.toLowerCase() === "paper") {
+            console.log("You lose");
+        }
+        else console.log("Invalid input");
+    }
+
+
+}
+
+console.log(playRound("paper", computerSelection));
