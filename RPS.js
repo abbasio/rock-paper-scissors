a0dfae39141b3b401bc2215824e574411fb9c8b8
abbasio <omarabbasi16@gmail.com>
2021-07-21 12:36:44 -0400

    Added score tracker and a 5x loop

--- RPS.js
+++ RPS.js
@@ -1,7 +1,5 @@
 /* COMPUTER SELECTION */
 
-
-
 function computerPlay(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
@@ -9,44 +7,63 @@ function computerPlay(arr) {
 const rps = ["rock", "paper", "scissors"];
 
 
+/* ROUND */
+
+let playerScore = 0;
+let computerScore = 0;
+
+
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === "rock") {
         if (computerSelection === "paper") {
-            return "You lose.";
+            computerScore++;
+            return `You lose. Paper beats rock. Player: ${playerScore}, Computer: ${computerScore}`;
         }
         else if (computerSelection === "scissors") {
-            return "You win!";
+            playerScore++;
+            return `You win! Rock beats scissors. Player: ${playerScore}, Computer: ${computerScore}`;
         }
     }
     if (playerSelection.toLowerCase() === "paper") {
         if (computerSelection === "rock") {
-            return "You win!";
+            playerScore++;
+            return `You win! Paper beats rock. Player: ${playerScore}, Computer: ${computerScore}`;
         }
         else if (computerSelection === "scissors") {
-            return "You lose.";
+            computerScore++;
+            return `You lose. Scissors beat paper. Player: ${playerScore}, Computer: ${computerScore}`;
         }
     }
     if (playerSelection.toLowerCase() === "scissors") {
         if (computerSelection === "rock") {
-            return "You lose.";
+            computerScore++;
+            return `You lose. Rock beats scissors. Player: ${playerScore}, Computer: ${computerScore}`;
         }
         else if (computerSelection === "paper") {
-            return "You win!";
+            playerScore++;
+            return `You win! Scissors beat paper. Player: ${playerScore}, Computer: ${computerScore}`;
         }
     }
     if (playerSelection.toLowerCase() === computerSelection) {
-        return "Tie game!";
+        return `Tie game! You both chose ${computerSelection}. Player: ${playerScore}, Computer: ${computerScore}`;
     }
     if (playerSelection.toLowerCase() != ("rock" || "paper" || "scissors")) {
-        return "Invalid input";
+        return `Invalid input, try again. Player: ${playerScore}, Computer: ${computerScore}`;
     }
 
 }
 
-const playerSelection = window.prompt("Make your selection:");
-const computerSelection = (computerPlay(rps));
+/* LOOP */
 
-console.log(playRound(playerSelection, computerSelection));
+for (let i = 0; i < 5; i++) {
+    let computerSelection = (computerPlay(rps));
+    let playerSelection = window.prompt("Choose Rock, Paper, or Scissors:");
+    playRound(playerSelection, computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
+}
+
+
+
 
 
 
