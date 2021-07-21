30e7fb87c44ad937d788e0ff2c5123a6c3c43bab
abbasio <omarabbasi16@gmail.com>
2021-07-21 13:34:58 -0400

    Added html display for game, fixed score tracking

--- RPS.js
+++ RPS.js
@@ -17,38 +17,38 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() === "rock") {
         if (computerSelection === "paper") {
             computerScore++;
-            return `You lose. Paper beats rock. Player: ${playerScore}, Computer: ${computerScore}`;
+            return `You lose. Paper beats rock. Player: ${playerScore / 2}, Computer: ${computerScore / 2}`;
         }
         else if (computerSelection === "scissors") {
             playerScore++;
-            return `You win! Rock beats scissors. Player: ${playerScore}, Computer: ${computerScore}`;
+            return `You win! Rock beats scissors. Player: ${playerScore / 2}, Computer: ${computerScore / 2}`;
         }
     }
     if (playerSelection.toLowerCase() === "paper") {
         if (computerSelection === "rock") {
             playerScore++;
-            return `You win! Paper beats rock. Player: ${playerScore}, Computer: ${computerScore}`;
+            return `You win! Paper beats rock. Player: ${playerScore / 2}, Computer: ${computerScore / 2}`;
         }
         else if (computerSelection === "scissors") {
             computerScore++;
-            return `You lose. Scissors beat paper. Player: ${playerScore}, Computer: ${computerScore}`;
+            return `You lose. Scissors beat paper. Player: ${playerScore / 2}, Computer: ${computerScore / 2}`;
         }
     }
     if (playerSelection.toLowerCase() === "scissors") {
         if (computerSelection === "rock") {
             computerScore++;
-            return `You lose. Rock beats scissors. Player: ${playerScore}, Computer: ${computerScore}`;
+            return `You lose. Rock beats scissors. Player: ${playerScore / 2}, Computer: ${computerScore / 2}`;
         }
         else if (computerSelection === "paper") {
             playerScore++;
-            return `You win! Scissors beat paper. Player: ${playerScore}, Computer: ${computerScore}`;
+            return `You win! Scissors beat paper. Player: ${playerScore / 2}, Computer: ${computerScore / 2}`;
         }
     }
     if (playerSelection.toLowerCase() === computerSelection) {
-        return `Tie game! You both chose ${computerSelection}. Player: ${playerScore}, Computer: ${computerScore}`;
+        return `Tie game! You both chose ${computerSelection}. Player: ${playerScore / 2}, Computer: ${computerScore / 2}`;
     }
     if (playerSelection.toLowerCase() != ("rock" || "paper" || "scissors")) {
-        return `Invalid input, try again. Player: ${playerScore}, Computer: ${computerScore}`;
+        return `Invalid input, try again. Player: ${playerScore / 2}, Computer: ${computerScore / 2}`;
     }
 
 }
@@ -59,7 +59,10 @@ for (let i = 0; i < 5; i++) {
     let computerSelection = (computerPlay(rps));
     let playerSelection = window.prompt("Choose Rock, Paper, or Scissors:");
     playRound(playerSelection, computerSelection);
-    console.log(playRound(playerSelection, computerSelection));
+    document.getElementById("game").innerHTML = playRound(playerSelection,computerSelection);
+    if (i == 4) {
+        document.getElementById("score").innerHTML = `Complete! Final score - Player: ${playerScore / 2}, Computer: ${computerScore / 2}. Refresh the page to play again.`
+    }
 }
 
 
