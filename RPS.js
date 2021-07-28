4a6ca1d2d4fdc858ad907b75f7353674f6bd9c13
abbasio <omarabbasi16@gmail.com>
2021-07-28 18:33:22 -0400

    Added full game functionality

--- RPS.js
+++ RPS.js
@@ -1,87 +1,125 @@
-/* COMPUTER SELECTION */
+//---------COMPUTER SELECTION
+
+const rps = ["rock", "paper", "scissors"];
 
 function computerPlay(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
-const rps = ["rock", "paper", "scissors"];
+//---------PLAYER SELECTION
 
+let input = document.querySelectorAll("input");
 
-/* ROUND */
+input.forEach((input) => {
+    input.addEventListener("click", () =>{
+        playerSelection = input.alt;
+        computerSelection = (computerPlay(rps));
+        playRound(playerSelection, computerSelection);
+    });
+});
+
+//---------SCORE AND RESET
 
 let playerScore = 0;
 let computerScore = 0;
 
-//function start() {
-   // const playGame = document.querySelector('#game');
-   // const button1 = document.createElement('button');
-    //const button2 = document.createElement('button');
-    //const button3 = document.createElement('button');
-    
-    //playGame.appendChild(button1);
-    //playGame.appendChild(button2);
-   // playGame.appendChild(button3);w
-//}
+function restart(parent) {
+    playerScore = 0;
+    computerScore = 0;
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
+
+//---------GAME
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === "rock") {
+
+
+    const score = document.querySelector('#score');
+    const updateScore = document.createElement('div');
+    const finalScore = document.createElement('p');
+    updateScore.classList.add('scoreLine');
+ 
+    if (playerSelection === "rock") {
         if (computerSelection === "paper") {
             computerScore++;
-            return `You lose. Paper beats rock. Player: ${playerScore}, Computer: ${computerScore}`;
+            updateScore.textContent = `You lose. Paper beats rock. Player: ${playerScore}, Computer: ${computerScore}`;
+            updateScore.style.color = "red";
+            
+            
         }
         else if (computerSelection === "scissors") {
             playerScore++;
-            return `You win! Rock beats scissors. Player: ${playerScore}, Computer: ${computerScore}`;
+            updateScore.textContent = `You win! Rock beats scissors. Player: ${playerScore}, Computer: ${computerScore}`;
+            updateScore.style.color = "green";
+            
         }
     }
-    if (playerSelection.toLowerCase() === "paper") {
+    if (playerSelection === "paper") {
         if (computerSelection === "rock") {
             playerScore++;
-            return `You win! Paper beats rock. Player: ${playerScore}, Computer: ${computerScore}`;
+            updateScore.textContent = `You win! Paper beats rock. Player: ${playerScore}, Computer: ${computerScore}`;
+            updateScore.style.color = "green";
         }
         else if (computerSelection === "scissors") {
             computerScore++;
-            return `You lose. Scissors beat paper. Player: ${playerScore}, Computer: ${computerScore}`;
+            updateScore.textContent = `You lose. Scissors beat paper. Player: ${playerScore}, Computer: ${computerScore}`;
+            updateScore.style.color = "red";
         }
     }
-    if (playerSelection.toLowerCase() === "scissors") {
+    if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
             computerScore++;
-            return `You lose. Rock beats scissors. Player: ${playerScore}, Computer: ${computerScore}`;
+            updateScore.textContent = `You lose. Rock beats scissors. Player: ${playerScore}, Computer: ${computerScore}`;
+            updateScore.style.color = "red";
         }
         else if (computerSelection === "paper") {
             playerScore++;
-            return `You win! Scissors beat paper. Player: ${playerScore}, Computer: ${computerScore}`;
+            updateScore.textContent = `You win! Scissors beat paper. Player: ${playerScore}, Computer: ${computerScore}`;
+            updateScore.style.color = "green";
         }
     }
-    if (playerSelection.toLowerCase() === computerSelection) {
-        return `Tie game! You both chose ${computerSelection}. Player: ${playerScore}, Computer: ${computerScore}`;
+    if (playerSelection === computerSelection) {
+        updateScore.textContent = `Tie game! You both chose ${computerSelection}. Player: ${playerScore}, Computer: ${computerScore}`;
+        
     }
-    if (playerSelection.toLowerCase() != ("rock" || "paper" || "scissors")) {
-        return `Invalid input, try again. Player: ${playerScore}, Computer: ${computerScore}`;
-    }
+    
+    score.appendChild(updateScore);
 
+//---------GAME OVER
+
+    if ((playerScore === 5) || (computerScore === 5)) {
+       
+        score.appendChild(updateScore);
+        if (confirm(`Game over!\nFinal Score - Player: ${playerScore}, Computer: ${computerScore}\nPress Cancel to end game, or press OK to restart`)) {
+            restart(score);
+         }
+        else {
+            if (playerScore === 5) {
+                finalScore.textContent = "You won! Congratulations!"
+                finalScore.style.color = "green"
+                finalScore.style.scale = 2;
+            }
+            if (computerScore === 5) {
+                finalScore.textContent = "You lost. Better luck next time!"
+                finalScore.style.color = "red"
+                finalScore.style.scale = 2;
+            }
+            
+            input.forEach ((input) => {
+                input.disabled = true;
+            });
+            score.appendChild(finalScore);
+        }
+        
+        
+
+    }
+   
 }
 
-/* LOOP */
-
-function game() {
-
-for (let i = 0; i < 5; i++) {
-    let computerSelection = (computerPlay(rps));
-    let playerSelection = window.prompt("Choose Rock, Paper, or Scissors:");
-    let roundResult = (playRound(playerSelection, computerSelection));
-    alert(roundResult);
-
-    if (i == 4) {
-        document.getElementById("score").textContent = `Complete! Final score - Player: ${playerScore}, Computer: ${computerScore}. Refresh the page to play again.`
-        document.getElementById("Button").disabled = true;
-    }
-}
-
-
-}
 
 
 
